9a349c1f96a70c69eae6b888d2c4456712e9c8b2
emanuelmath <emanueldominguezm4@gmail.com>
2025-09-23 20:01:23 -0600

    Entity de sesión agregada.

---
+++ app/src/main/java/com/example/paradiseresorts/data/database/entities/SessionEntity.kt
@@ -0,0 +1,14 @@
+package com.example.paradiseresorts.data.database.entities
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity("session",
+    foreignKeys = [ForeignKey(UserEntity::class, parentColumns = ["dui"], childColumns = ["dui"])])
+data class SessionEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val dui: String,
+    val sessionDate: String
+)
